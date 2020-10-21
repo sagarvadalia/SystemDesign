696a6a679af1a69a1c1ee9d2314e1b8b23f473ba
sagarvadalia <sv344@cornell.edu>
2020-10-21 16:04:41 -0400

    class database model

---
+++ server/db/models/class.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto')
+const {INTEGER} = require('sequelize')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Class = db.define('class', {
+  classID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+  },
+  classSection: {
+    notEmpty: true,
+    type: Sequelize.STRING,
+  },
+  numOfSeats: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
+})
+
+module.exports = Department
